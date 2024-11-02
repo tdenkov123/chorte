4ee2786889b39f65141f82714596e0106b0deb81
tdenkov123 <71324191+tdenkov123@users.noreply.github.com>
2024-11-02 15:17:25 +0300

    first commit

---
+++ src/Chorte.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <argparse>
+#include <pugixml.hpp>
+#include <connector.h>
+#include <tcp_connector.h>
+
+
+#include <iostream>
+
+class Chorte {
+    public:
+    
+    private:
+
+};
