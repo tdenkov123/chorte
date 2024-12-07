a7e425f1dcd2e8b85f070a240cbe02f907e0d566
tdenkov123 <71324191+tdenkov123@users.noreply.github.com>
2024-12-07 19:26:59 +0300

    working connections

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
+#include <iostream>
+#include "RuntimeEnvironment.h"
+
 
 int main(int argc, char* argv[]) {
-    
-    
+    FBContainer container;
+    container.add_block("START", std::make_shared<START>("START"));
+    container.set_start_block("START");
+    container.add_block("PrintFB", std::make_shared<PrintFB>("PrintFB"));
+    container.create_connection("START", "PrintFB");
+
+    RuntimeEnvironment runtime_env(container);
+    runtime_env.run();
+
     return 0;
 }
