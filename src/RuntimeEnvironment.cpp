672f50288d74ccf799eff01d19e405aa21cb3403
tdenkov123 <71324191+tdenkov123@users.noreply.github.com>
2024-12-14 17:21:55 +0300

    runtime working

--- src/RuntimeEnvironment.cpp
+++ src/RuntimeEnvironment.cpp
@@ -21,34 +21,34 @@ void RuntimeEnvironment::process() {
         auto current_fb = queue.front();
         queue.pop();
 
-
         current_fb->execute();
         std::cout << current_fb->get_name() << " executed\n";
 
-        for (const auto& [from, to] : container.get_connections()) {
-            auto pos_from = from.find('.');
-            auto pos_to = to.find('.');
-            std::string from_fb = from.substr(0, pos_from);
-            std::string from_port = from.substr(pos_from + 1);
-            std::string to_fb = to.substr(0, pos_to);
-            std::string to_port = to.substr(pos_to + 1);
+        const auto& event_connections = container.get_event_connections();
+        auto event_range = event_connections.equal_range(current_fb->get_name());
 
-            if (from_fb == current_fb->get_name()) {
-                auto next_fb = container.get_block(to_fb);
-                if (next_fb) {
-                    if (current_fb->get_event_outputs().count(from_port)) {
-                        bool req = current_fb->get_event_outputs().at(from_port);
-                        if (req) {
-                            current_fb->set_event_input("REQ", false);
-                            current_fb->set_event_output("CNF", true);
-                        }
-                    }
-                    if (current_fb->get_data_outputs().count(from_port)) {
-                        auto value = current_fb->get_data_outputs().at(from_port);
-                        next_fb->set_data_input(to_port, value);
-                    }
-                std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        const auto& data_connections = container.get_event_connections();
+        auto data_range = data_connections.equal_range(current_fb->get_name());
+
+        for (auto it = data_range.first; it != data_range.second; ++it) {
+            auto conn = it->second;
+            auto next_fb = container.get_block(conn.to_fb);
+            if (next_fb) {
+                next_fb->set_event_input("REQ", true);
+                current_fb->set_event_input("REQ", false);
+                current_fb->set_event_input("CNF", false);
                 queue.push(next_fb);
+            }
+        }
+
+        for (auto& it = data_range.first; it != data_range.second; ++it) {
+            auto conn = it->second;
+            auto next_fb = container.get_block(conn.to_fb);
+            if (next_fb) {
+                auto outputs = current_fb->get_data_outputs();
+                if (outputs.size()) {
+                    auto value = outputs.find(conn.from_port);
+                    if (value != outputs.end()) next_fb->set_data_input(conn.to_port, value->second);   
                 }
             }
         }
